bd3b0557edbe529a3de62b113a3761f534e3c5c6
Tyler Chao <cz848@qq.com>
2018-02-22 20:10:49 +0800

    getintouchform

--- steelcase/assets/js/main.js
+++ steelcase/assets/js/main.js
@@ -1,1 +1,463 @@
-$(document).mobile(),function(){function a(a){$(".angle"+l).removeClass("show"),l=this.value,$(".angle"+l).addClass("show")}function b(a){switch(this.dataset.direction){case"next":if(i===f)return!1;i++,$("#color"+i).addClass("show");break;case"prev":if(1===i)return!1;$("#color"+i).removeClass("show"),i--}$("#controls").attr("class","controls"+$("#color"+i).attr("class").replace("color","")),i===f||1===i?this.disabled=!0:i!==f-1&&2!==i||($(this).siblings(".arrow")[0].disabled=!1)}function c(a){switch(this.dataset.direction){case"next":if(j===g)return!1;j++,$("#pattern"+j).addClass("show");break;case"prev":if(1===j)return!1;$("#pattern"+j).removeClass("show"),j--}j===g||1===j?this.disabled=!0:j!==g-1&&2!==j||($(this).siblings(".arrow")[0].disabled=!1)}function d(a){switch(this.dataset.direction){case"next":if(k===h)return!1;k++,$("#range"+k).addClass("show");break;case"prev":if(1===k)return!1;$("#range"+k).removeClass("show"),k--}k===h||1===k?this.disabled=!0:k!==h-1&&2!==k||($(this).siblings(".arrow")[0].disabled=!1)}function e(a){for(var b=0,c=1;c<a.length;c++)a[c]<a[b]&&(b=c);return b}var f=6,g=5,h=3,i=1,j=1,k=1,l=4;$("#panel1 .arrow").click(b),$("#panel2 .arrow").click(c),$("#panel3 .arrow").click(d),$("#angleSlider").on("input",a),window.onload=function(){if(document.body.className="","#next"===location.hash){var a=70-window.innerHeight;$("#nav .swiper-btn-next").parent().addClass("active").parent().css({"-webkit-transition":"none",transition:"none","-webkit-transform":"translate(0, "+a+"px)",transform:"translate(0, "+a+"px)"})}};var m=new pageSwitch("#slider .swiper-wrapper",{duration:1e3,start:0,direction:0,loop:!0,ease:"ease",transition:"scrollCoverX",mouse:!1,mousewheel:!1,arrowkey:!1}),n=$("#slider .swiper-slide .text");$("#slider .slide-nav a").on("click",function(a){m.slide(m.current+1*("next"===this.rel?1:-1))}),n[0].classList.add("active"),m.on("before",function(a,b){n[a].classList.remove("active")}).on("after",function(a,b){n[a].classList.add("active")});var o=new Swiper("#fullpage",{direction:"vertical",effect:"slide",slidesPerView:"auto",initialSlide:0,nextButton:".swiper-btn-next",hashnav:!0,hashnavWatchState:!0,history:!1}),p=$("#nav");p.clone(!0).attr("id","nav1").hide().css("position","relative").appendTo(".placeholder:eq(1)");var q=0,r=0;o.slides.eq(0).on("touchstart",function(a){q=r=a.originalEvent.targetTouches[0].pageY}).on("touchmove",function(a){if(r=a.originalEvent.targetTouches[0].pageY,q!=r)return!1}),o.on("sliderMove",function(a){o.touches.startY<o.touches.currentY&&1===o.activeIndex&&(p.hide().css({transform:"","-webkit-transform":""}),$("#nav1").show())}),$(window).on("hashchange",function(a){if(0===o.activeIndex)$("#nav1").show().css({transform:"translate(0, -70px)",webkitTransform:"translate(0, -70px)","transition-duration":".1s"}).on("transitionend webkitTransitonEnd",function(a){$(this).hide().css({transform:"","transition-duration":""}),p.show().css({transform:"","-webkit-transform":""})});else if(1===o.activeIndex){var b=70-window.innerHeight;$("#nav .swiper-btn-next").parent().addClass("active").parent().show().css({"-webkit-transition":"none",transition:"none","-webkit-transform":"translate(0, "+b+"px)",transform:"translate(0, "+b+"px)"})}}),$("#nav .swiper-btn-next").on("click",function(a){var b=70-window.innerHeight;$(this).parent().addClass("active").parent().css({transform:"translate(0, "+b+"px)","-webkit-transform":"translate(0, "+b+"px)"}),$("#nav1 .swiper-btn-next").parent().addClass("active")});var s=new Swiper("#subSlider",{direction:"vertical",effect:"slide",autoHeight:!0,nested:!0,slidesPerView:"auto",observer:!0,freeModeMomentumBounce:!1});s.on("touchStart",function(a){s.snapGrid[4]=s.snapGrid[3]+s.slides[3].offsetHeight-window.innerHeight+70}).on("touchMove",function(a){3===s.activeIndex&&("next"===s.swipeDirection||"prev"===s.swipeDirection&&s.translate<-1238)?s.params.freeMode=!0:s.params.freeMode=!1}).on("touchEnd",function(a){s.translate>-1238&&(s.params.freeMode=!1)}).on("transitionEnd",function(a){if(s.translate>-1238){for(var b=[],c=0;c<s.slidesGrid.length;c++)b.push(Math.abs(s.translate+s.slidesGrid[c]));s.slideTo(e(b)),s.params.freeMode=!1}}),$("#chair .tab li a").on("click",function(a){a.preventDefault(),$(this).parent().addClass("active").siblings(".active").removeClass("active"),$(this.getAttribute("href")).css("z-index",2).addClass("fade-in").on("animationend",function(){$(this).siblings(".fade-in").removeClass("fade-in").off("animationend")}).siblings(".fade-in").css("z-index","")}),$(".switcher .btn-group button").click(function(a){$(this).hasClass("active")||($(this).addClass("active").siblings(".active").removeClass("active"),$(this).parents(".content").find(".black").toggleClass("show"))}),$(".switcher input[type=checkbox]").change(function(a){$(this).parents(".content").find(".chrome").toggleClass("show")});for(var t=0,q=0,r=0,u=8.5,v=274-2*u-22,w=4,x=Math.floor(v/7*10)/10,y=(window.innerWidth-274)/2,z=[],A=0;A<8;A++)z[A]=x*A+u;$("#controls").on("touchstart",function(a){q=r=a.originalEvent.targetTouches[0].pageY}).on("touchmove",function(a){if(t=a.originalEvent.targetTouches[0].pageX-y,r=a.originalEvent.targetTouches[0].pageY,Math.abs(r-q)&&(a.preventDefault(),a.stopPropagation()),t<u||t>v+u+11)return!1;var b=t;b>z[w-1]+x/2+11?$("#angleSlider").val(++w).trigger("input"):b<z[w-1]-x/2+11&&$("#angleSlider").val(--w).trigger("input")}).on("touchend",function(a){})}();
+//Open the Mobile Front-end Codebase JS
+$(document).mobile();
+
+(function() {
+
+  var nrColors = 6;
+  var nrAngels = 8;
+  var nrPatterns = 5;
+  var nrRanges = 3;
+  var currentColor = 1;
+  var currentPattern = 1;
+  var currentRange = 1;
+  var currentAngle = 4;
+
+  // Init Event Handlers
+  $('#panel1 .arrow').click(changeColor);
+  $('#panel2 .arrow').click(changePattern);
+  $('#panel3 .arrow').click(changeRange);
+  $('#angleSlider').on('input', changeAngle);
+
+  // var margin = ((window.innerWidth - 189) / 2);
+  // $('.color section').css('left', margin + 'px');
+  // $('#shadow').css('left', margin - 74 + 'px');
+
+
+  /**
+   * Handle Angle change
+   * @param {*} e - jQuery event passed from slider
+   */
+  function changeAngle(e) {
+    // Hide current position
+    $('.angle' + currentAngle).removeClass('show');
+
+    // Set new angle based on slider position
+    currentAngle = this.value;
+
+    // Show the new angle
+    $('.angle' + currentAngle).addClass('show');
+  }
+
+  /**
+   * Handle Color Change based on CTA id 
+   */
+  function changeColor(e) {
+    switch (this.dataset.direction) {
+      case 'next':
+        // check if end of colours are reached
+        if (currentColor === nrColors) return false;
+
+        // increase color count
+        currentColor++;
+
+        // show new colour
+        $('#color' + currentColor).addClass('show');
+        break;
+
+      case 'prev':
+        // check if beginning of color range is reached        
+        if (currentColor === 1) return false;
+
+        // remove current colour
+        $('#color' + currentColor).removeClass('show');
+
+        // decrease colour count;
+        currentColor--;
+        break;
+    }
+
+    $('#controls').attr('class', 'controls' + $('#color' + currentColor).attr('class').replace('color', ''));
+
+    if (currentColor === nrColors || currentColor === 1) {
+      this.disabled = true;
+    }
+    else if (currentColor === nrColors - 1 || currentColor === 2) {
+      $(this).siblings('.arrow')[0].disabled = false;
+    }
+  }
+
+  /**
+   * Handle Pattern Change based on CTA id 
+   */
+  function changePattern(e) {
+    switch (this.dataset.direction) {
+      case 'next':
+        // check if end of colours are reached
+        if (currentPattern === nrPatterns) return false;
+
+        // increase pattern count
+        currentPattern++;
+
+        // show new colour
+        $('#pattern' + currentPattern).addClass('show');
+        break;
+
+      case 'prev':
+        // check if beginning of pattern range is reached        
+        if (currentPattern === 1) return false;
+
+        // remove current colour
+        $('#pattern' + currentPattern).removeClass('show');
+
+        // decrease colour count;
+        currentPattern--;
+        break;
+    }
+
+    if (currentPattern === nrPatterns || currentPattern === 1) {
+      this.disabled = true;
+    }
+    else if (currentPattern === nrPatterns - 1 || currentPattern === 2) {
+      $(this).siblings('.arrow')[0].disabled = false;
+    }
+  }
+
+  /**
+   * Handle Range Change based on CTA id 
+   */
+  function changeRange(e) {
+    switch (this.dataset.direction) {
+      case 'next':
+        // check if end of colours are reached
+        if (currentRange === nrRanges) return false;
+
+        // increase range count
+        currentRange++;
+
+        // show new colour
+        $('#range' + currentRange).addClass('show');
+          // .find('h1').each(function() {
+          //   $(this).css('left', (window.innerWidth - $(this).width()) / 2);
+          // });
+
+        break;
+
+      case 'prev':
+        // check if beginning of range range is reached        
+        if (currentRange === 1) return false;
+
+        // remove current colour
+        $('#range' + currentRange).removeClass('show');
+
+        // decrease colour count;
+        currentRange--;
+        break;
+    }
+
+    if (currentRange === nrRanges || currentRange === 1) {
+      this.disabled = true;
+    }
+    else if (currentRange === nrRanges - 1 || currentRange === 2) {
+      $(this).siblings('.arrow')[0].disabled = false;
+    }
+  }
+
+
+  // Index loading and first screen ads.
+  window.onload = function() {
+    document.body.className = '';
+    if(location.hash === '#next') {
+      var y = 70 - window.innerHeight;
+      $('#nav .swiper-btn-next').parent()
+        .addClass('active')
+        .parent()
+        .css({
+          '-webkit-transition': 'none',
+          'transition': 'none',
+          '-webkit-transform': 'translate(0, ' + y + 'px)',
+          'transform': 'translate(0, ' + y + 'px)',
+        });
+      // swiper.detachEvents();
+    }
+  };
+
+  // the first screen ads slider
+  var slider = new pageSwitch('#slider .swiper-wrapper', {
+      duration: 1000,
+      start: 0,
+      direction: 0,
+      loop: true,
+      ease: 'ease',
+      transition: 'scrollCoverX',
+      mouse: false,
+      mousewheel: false,
+      arrowkey: false
+    }),
+    text = $('#slider .swiper-slide .text');
+
+  $('#slider .slide-nav a').on('click', function(e) {
+    slider.slide(slider.current + 1 * (this.rel === 'next' ? 1 : -1));
+  });
+  text[0].classList.add('active');
+  slider.on('before', function(m, n) {
+    text[m].classList.remove('active');
+  }).on('after', function(m, n) {
+    text[m].classList.add('active');
+  });
+
+  // first fullscreen swiper
+  var swiper = new Swiper('#fullpage', {
+    direction: 'vertical',
+    effect: 'slide',
+    slidesPerView: 'auto',
+    initialSlide: 0,
+    nextButton: '.swiper-btn-next',
+    hashnav: true,
+    hashnavWatchState: true,
+    history: false,
+  });
+
+  var nav = $('#nav');
+  // duplicate nav to nav1
+  nav
+    .clone(true)
+    .attr('id', 'nav1')
+    .hide()
+    .css('position', 'relative')
+    .appendTo('.placeholder:eq(1)');
+
+  // disable the first screen scroll up and down.
+  // var startY = 0;
+  // var currentY = 0;
+
+  swiper.lockSwipes();
+  // swiper.slides.eq(0)
+  //   .on('touchstart', function(e) {
+  //     startY = currentY = e.originalEvent.targetTouches[0].pageY;
+  //   })
+  //   .on('touchmove', function(e) {
+  //     currentY = e.originalEvent.targetTouches[0].pageY;
+
+  //     if (startY != currentY) {
+  //       return false;
+  //     }
+  //   });
+
+  // slide to first ads screen?
+  swiper
+    // .on('touchStart', function(e) {
+    // })
+    .on('sliderMove', function(e) {
+      if (swiper.touches.startY < swiper.touches.currentY) {
+        if (swiper.activeIndex === 1) {
+          nav
+            .hide()
+            .css({
+              'transform': '',
+              '-webkit-transform': ''
+            });
+          $('#nav1').show();
+        }
+      }
+    })
+    /*.on('transitionEnd', function(e) {
+      if (swiper.activeIndex === 0) {
+        $('#nav1')
+          .css({
+            'transform': 'translate(0, -70px)',
+            'webkitTransform': 'translate(0, -70px)',
+            'transition-duration': '.1s',
+          })
+          .on('transitionend webkitTransitonEnd', function(e) {
+            $(this)
+              .hide()
+              .css({
+                'transform': '',
+                'transition-duration': '',
+              });
+            nav.show();
+          });
+      }
+    });*/
+
+  $(window).on('hashchange', function(e) {
+      if (swiper.activeIndex === 0) {
+        $('#nav1')
+          .show()
+          .css({
+            'transform': 'translate(0, -70px)',
+            'webkitTransform': 'translate(0, -70px)',
+            'transition-duration': '.1s',
+          })
+          .on('transitionend webkitTransitonEnd', function(e) {
+            $(this)
+              .hide()
+              .css({
+                'transform': '',
+                'transition-duration': '',
+              });
+            nav
+              .show()
+              .css({
+                'transform': '',
+                '-webkit-transform': ''
+              });
+          });
+      }
+      else if (swiper.activeIndex === 1) {
+        var y = 70 - window.innerHeight;
+        $('#nav .swiper-btn-next').parent()
+          .addClass('active')
+          .parent()
+          .show()
+          .css({
+            '-webkit-transition': 'none',
+            'transition': 'none',
+            '-webkit-transform': 'translate(0, ' + y + 'px)',
+            'transform': 'translate(0, ' + y + 'px)',
+          });
+      }
+  });
+
+  // nav's first button
+  $('#nav .swiper-btn-next')
+    .on('touchend', function(e) {
+      swiper.unlockSwipeToNext();
+    })
+    .on('click', function(e) {
+      var y = 70 - window.innerHeight;
+      $(this).parent()
+        .addClass('active')
+        .parent()
+        .css({
+          'transform': 'translate(0, ' + y + 'px)',
+          '-webkit-transform': 'translate(0, ' + y + 'px)'
+        });
+        $('#nav1 .swiper-btn-next').parent().addClass('active');
+      // swiper.detachEvents();
+    });
+
+
+  // second screen swiper
+  var subs = new Swiper('#subSlider', {
+    direction: 'vertical',
+    effect: 'slide',
+    autoHeight: true,
+    nested: true,
+    slidesPerView: 'auto',
+    observer: true,
+    // freeMode: true,
+    freeModeMomentumBounce: false,
+  });
+
+  // the second & third & fourth screen has difference height
+  subs
+    .on('touchStart', function(e) {
+      subs.snapGrid[4] = subs.snapGrid[3] + subs.slides[3].offsetHeight - window.innerHeight + 70;
+    })
+    .on('touchMove', function(e) {
+
+      if (subs.activeIndex === 3 && (subs.swipeDirection === 'next' || (subs.swipeDirection === 'prev' && subs.translate < -1238))) {
+        subs.params.freeMode = true;
+      }
+      else {
+        subs.params.freeMode = false;
+      }
+    })
+    .on('touchEnd', function(e) {
+      if (subs.translate > -1238) {
+        subs.params.freeMode = false;
+      }
+    })
+    .on('transitionEnd', function(e) {
+      if (subs.translate > -1238) {
+        var arr = [];
+        for(var i = 0; i < subs.slidesGrid.length; i++) {
+          arr.push(Math.abs(subs.translate + subs.slidesGrid[i]));
+        }
+        subs.slideTo(smallest(arr));
+        subs.params.freeMode = false;
+      }
+  });
+
+  function smallest(a) {
+    var lowest = 0;
+    for (var i = 1; i < a.length; i++) {
+      if (a[i] < a[lowest]) lowest = i;
+    }
+    return lowest;
+  }
+
+
+  // the tabs switch
+  $('#chair .tab li a').on('click', function(e) {
+    e.preventDefault();
+    $(this).parent().addClass('active').siblings('.active').removeClass('active');
+    $(this.getAttribute('href'))
+      .css('z-index', 2)
+      .addClass('fade-in')
+      .on('animationend', function() {
+        $(this).siblings('.fade-in').removeClass('fade-in').off('animationend');
+      })
+      // .find('h1')
+      //   .css('left', (window.innerWidth - $('#range1 h1').width()) / 2)
+      // .end()
+      .siblings('.fade-in').css('z-index', '');
+        // .removeClass('fast')
+        // .addClass('fade-out')
+  });
+
+  // the more colors frame's switch
+  $('.switcher .btn-group button').click(function(e) {
+    if ($(this).hasClass('active')) return;
+    $(this).addClass('active').siblings('.active').removeClass('active');
+    $(this).parents('.content').find('.black').toggleClass('show');
+  });
+
+  $('.switcher input[type=checkbox]').change(function(e) {
+    $(this).parents('.content').find('.chrome').toggleClass('show');
+  });
+
+
+  // the curved angle slider
+  var currentX = 0;
+  var startY = 0;
+  var currentY = 0;
+  var start = 8.5;
+  var end = 274 - start * 2 - 22;
+  var angNum = 4;
+  var distance = Math.floor(end / 7 * 10) / 10;
+  var iteration = 0;
+  var offsetX = (window.innerWidth - 274) / 2;
+  var grid = [];
+  for (var i = 0; i < 8; i++) {
+    grid[i] = distance * i + start;
+  }
+  $('#controls')
+    .on('touchstart', function(e) {
+      // startX = currentX = e.originalEvent.targetTouches[0].pageX - offsetX;
+      startY = currentY = e.originalEvent.targetTouches[0].pageY;
+    })
+    .on('touchmove', function(e) {
+      currentX = e.originalEvent.targetTouches[0].pageX - offsetX;
+      currentY = e.originalEvent.targetTouches[0].pageY;
+      if (Math.abs(currentY - startY)) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+
+      if (currentX < start || currentX > end + start + 11) {
+        return false;
+      }
+
+      var diff = currentX;
+      // for (var i = 0; i < 8; i++) {
+      // if (diff > 0 && diff > distance / 2) {
+      //   $('#angleSlider').val(++angNum).trigger('input');
+      //   // startX = grid[angNum - 1];
+      // }
+      // else if (diff < 0 && diff < -distance / 2) {
+      //   $('#angleSlider').val(--angNum).trigger('input');
+      //   // startX = grid[angNum - 1];
+      // }
+      if (diff > grid[angNum - 1] + distance / 2 + 11) {
+        $('#angleSlider').val(++angNum).trigger('input');
+      }
+      else if (diff < grid[angNum - 1] - distance / 2 + 11) {
+        $('#angleSlider').val(--angNum).trigger('input');
+      }
+      // }
+    })
+    .on('touchend', function(e) {
+    });
+})();
